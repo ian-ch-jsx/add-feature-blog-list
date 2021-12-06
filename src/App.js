27e1e0ff917f034664f3846bc5fc944a79af5a44
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-06 14:25:10 -0800

    blog posts rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
@@ -10,13 +11,16 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await getBlogs();
-      console.log(data);
+      setBlogs(data);
     };
     fetchData();
   }, []);
   return (
     <section className="main">
       <Header />
+      {blogs.map((posts) => {
+        return <BlogCard key={posts.id} {...posts} />;
+      })}
       <Footer />
     </section>
   );
